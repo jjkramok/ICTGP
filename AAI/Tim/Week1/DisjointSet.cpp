10a4dff48a164d98c79e4208c26c2a0f9b421a6d
jjkramok <jjkramok@gmail.com>
2018-01-31 15:35:39 +0100

    finished the disjoint set class

--- AAI/Tim/Week1/DisjointSet.cpp
+++ AAI/Tim/Week1/DisjointSet.cpp
@@ -3,6 +3,7 @@
 //
 
 #include <cstdlib>
+#include <cstdio>
 #include "DisjointSet.h"
 
 class DisjointSets
@@ -28,20 +29,53 @@ class DisjointSets
     }
 
     int DisjointSet::Union(int elem1, int elem2) {
+        int parent1 = this->Find(elem1);
+        int parent2 = this->Find(elem2);
 
+        if (parent1 == parent2) {
+            // Both elements already belong to the same set.
+            return 1;
+        }
+
+        if (setArray[parent1] > setArray[parent2]) {
+            // Merge smaller tree (parent2) into bigger tree (parent1).
+            setArray[parent1] += setArray[parent2]; // Increase size of the bigger tree by size of the smaller tree.
+            setArray[parent2] = parent1; // Merge the smaller tree by marking parent1 as parent of the smaller tree.
+        } else {
+            // Merge smaller tree (parent1) into bigger tree (parent2).
+            setArray[parent2] += setArray[parent1]; // Increase size of the bigger tree by size of the smaller tree.
+            setArray[parent1] = parent2; // Merge the smaller tree by marking parent2 as parent of the smaller tree.
+        }
+        amountOfSets--;
+        return 0;
     }
 
     int DisjointSet::Find(int elem) {
         int parent = setArray[elem];
-        if (parent == -1) {
+
+        if (parent < 0) {
+            // Element is also parent of his own set, return itself.
             return elem;
         } else {
-            Find(parent);
+            int parentOfParent = Find(parent);
+            setArray[elem] = parentOfParent;
+            return parentOfParent;
         }
     }
 
     char *DisjointSet::ToString() {
+        char *res = (char*) calloc(setArraySize * 5 + 1 , sizeof(char));
 
+        for (int i = 0; i < setArraySize; i++) {
+            sprintf(&res[i * 5], "%d", setArray[i]);
+            for (int j = i * 5; j < 5 * i + 5; j++) {
+                if (res[j] == '\0') {
+                    res[j] = ' ';
+                }
+            }
+        }
+        res[setArraySize * 5 + 1] = '\0';
+        return res;
     }
 
 };
--- AAI/Tim/Week1/DisjointSet.h
+++ AAI/Tim/Week1/DisjointSet.h
@@ -5,9 +5,13 @@
 #ifndef TIM_DISJOINTSET_H
 #define TIM_DISJOINTSET_H
 
-
+/**
+ * A Simple Disjoint set implemented using an up-tree using an array.
+ */
 class DisjointSet {
 public:
+    int amountOfSets;
+
     /**
      * Generates an identity disjointset of given size.
      * @param size
@@ -19,16 +23,26 @@ public:
      * Merges two disjoint sets
      * @param elem1
      * @param elem2
-     * @return success status (0 success)
+     * @return status (0 success) (1 same parent)
      */
     int Union(int elem1, int elem2);
+
+    /**
+     * Finds the root node of the given node identifier.
+     * @param elem node identifier of which the root is requested to be found.
+     * @return root identifier
+     */
     int Find(int elem);
+
+    /**
+     * Builds a string representation of this disjoint set
+     * @return string representation of the disjoint set
+     */
     char *ToString();
 
 protected:
     int *setArray;
     int setArraySize;
-    int amountOfSets;
 };
 
 
