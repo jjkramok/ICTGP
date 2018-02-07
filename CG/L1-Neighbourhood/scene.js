62d9a614ecb9c92a870748669797492250b5e53c
jjkramok <jjkramok@gmail.com>
2018-02-07 09:25:40 +0100

    started working on first homework assignment for CG

---
+++ CG/L1-Neighbourhood/scene.js
@@ -0,0 +1,44 @@
+// Create scene
+var scene = new THREE.Scene();
+
+// Create camera
+var camera = new THREE.PerspectiveCamera(
+	75, // fov — Camera frustum vertical field of view.
+	window.innerWidth/window.innerHeight, // aspect — Camera frustum aspect ratio.
+	0.1, // near — Camera frustum near plane.
+	1000); // far — Camera frustum far plane. 
+
+// Create renderer
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+var geometry = new THREE.BoxBufferGeometry( 1, 1, 1 );
+// var material = new THREE.MeshNormalMaterial();
+
+var texture = new THREE.TextureLoader().load( '/ModelsAndTextures/Yellobrk.bmp' );
+var geometry = new THREE.BoxBufferGeometry( 200, 200, 200 );
+var material = new THREE.MeshBasicMaterial( { map: texture } );
+
+var cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+// Move camera from center
+camera.position.x = 2;
+camera.position.y = 1; // height
+camera.position.z = 5;
+
+var clock = new THREE.Clock();
+
+var render = function () {
+    requestAnimationFrame(render);
+    var delta = clock.getDelta();
+
+    cube.rotation.x += 1.2 * delta;
+    cube.rotation.y += 1.2 * delta;
+    cube.rotation.z += .2 * delta;
+
+    renderer.render(scene, camera);
+};
+
+render();
