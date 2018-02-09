2d95f1f012e661265f0a7258905e122fadff265b
Sjoerd Dekker <sjoerdherxen@outlook.com>
2018-02-09 10:41:29 +0100

    added cg vector/matrix and tictactoe

---
+++ AAI/SjoerdCS/TicTacToe/TicTacToe.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SjoerdCS.TicTacToe
+{
+	class TicTacToe
+	{
+		public enum Marble { EMPTY, CROSS, ZERO };
+
+		/**
+         * 1 2 3
+         * 4 5 6
+         * 7 8 9
+         * */
+
+		int[] history;
+		Marble[] board;
+		int count;          //  number of marbles on the board
+
+		public TicTacToe()
+		{
+			history = new int[9];
+			board = new Marble[10];
+			count = 0;
+		}
+
+		public bool InInsertingState()
+		{
+			return count < 9;
+		}
+
+		public bool DoMove(Marble marble, int position)
+		{
+			if (count < 9)
+			{
+				board[position] = marble;
+				history[count] = position;
+				count++;
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool DoMove(int positionFrom, int positionTo)
+		{
+			board[positionTo] = board[positionFrom];
+			board[positionFrom] = Marble.EMPTY;
+			return true;
+		}
+
+		public void UndoMove()
+		{
+			if (count == 0)
+			{
+				return;
+			}
+			count--;
+			board[history[count]] = Marble.EMPTY;
+		}
+
+		public bool IsEmpty(int position)
+		{
+			return board[position] == Marble.EMPTY;
+		}
+
+		public Marble Get(int position)
+		{
+			return board[position];
+		}
+
+		public Marble Reverse(Marble marble)
+		{
+			switch (marble)
+			{
+				case Marble.CROSS:
+					return Marble.ZERO;
+				case Marble.ZERO:
+					return Marble.CROSS;
+				default:
+					return marble;
+			}
+		}
+
+		public bool IsWinner(Marble m)
+		{
+			if (B(m, 1) && B(m, 2) && B(m, 3))
+				return true;
+			if (B(m, 4) && B(m, 5) && B(m, 6))
+				return true;
+			if (B(m, 7) && B(m, 8) && B(m, 9))
+				return true;
+
+			if (B(m, 1) && B(m, 4) && B(m, 7))
+				return true;
+			if (B(m, 2) && B(m, 5) && B(m, 8))
+				return true;
+			if (B(m, 3) && B(m, 6) && B(m, 9))
+				return true;
+
+			if (B(m, 1) && B(m, 5) && B(m, 9))
+				return true;
+			if (B(m, 3) && B(m, 5) && B(m, 7))
+				return true;
+
+			return false;
+		}
+
+		private bool B(Marble marble, int position)
+		{
+			return board[position] == marble;
+		}
+
+
+
+		/***
+         * methods to be implemented for finding the best move
+         * */
+
+		public bool FindAndDoBestComputerMove(Marble m)
+		{
+			int bestMoveValue = -100;
+			int bestMove = 0;
+			for (int i = 1; i <= 9; i++)
+			{
+				if (IsEmpty(i))
+				{
+					DoMove(m, i);
+					int value = ComputerMove(Reverse(m), m, false, 0);
+					Console.WriteLine(value);
+					if (value > bestMoveValue)
+					{
+						bestMove = i;
+						bestMoveValue = value;
+					}
+					UndoMove();
+				}
+			}
+			if (bestMove == 0)
+			{
+				return false;
+			}
+
+			board[bestMove] = m;
+
+			return true;
+		}
+
+		private int ComputerMove(Marble player, Marble originalPlayer, bool maximize, int depth)
+		{
+			if (IsWinner(originalPlayer))
+			{
+				Console.WriteLine(15);
+				return 15;
+			}
+			if (IsWinner(Reverse(originalPlayer)))
+			{
+				Console.WriteLine(-15);
+				return -15;
+			}
+			if (count == 9)
+			{
+				Console.WriteLine(0);
+				return 0;
+			}
+			int bestValue = maximize ? -100 : 100;
+			for (int i = 1; i <= 9; i++)
+			{
+				if (IsEmpty(i))
+				{
+					DoMove(player, i);
+					int value = ComputerMove(Reverse(player), originalPlayer, !maximize, depth + 1);
+					if (bestValue < value == maximize)
+					{
+						bestValue = value;
+					}
+					UndoMove();
+				}
+			}
+			return bestValue;
+		}
+
+		public string Str { get { return ToString(); } }
+
+		public override string ToString()
+		{
+			var result = "";
+			for (int i = 1; i <= 9; i++)
+			{
+				if (i % 3 == 1)
+				{
+					result += "\n";
+				}
+				switch (board[i])
+				{
+					case Marble.CROSS:
+						result += "X";
+						break;
+					case Marble.ZERO:
+						result += "O";
+						break;
+					case Marble.EMPTY:
+						result += ".";
+						break;
+				}
+			}
+			return result.Trim();
+		}
+	}
+}
---
+++ CG/Sjoerd/CS/CG/Matrix.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CG
+{
+	class Matrix
+	{
+		public float[,] Values;
+		public int Rows { get; private set; }
+		public int Columns { get; private set; }
+
+		public Matrix(int squareSize)
+		{
+			Values = new float[squareSize, squareSize];
+			Rows = squareSize;
+			Columns = squareSize;
+		}
+
+		public Matrix(int rows, int columns)
+		{
+			Values = new float[rows, columns];
+			Rows = rows;
+			Columns = columns;
+		}
+
+		public Matrix(int rows, int columns, float[] values)
+		{
+			Values = new float[rows, columns];
+			Rows = rows;
+			Columns = columns;
+		}
+
+		public static Matrix operator +(Matrix m1, Matrix m2)
+		{
+			if (m1.Rows != m2.Rows || m1.Columns != m2.Columns)
+			{
+				throw new Exception("Matrixes are not the same size");
+			}
+			var result = new Matrix(m1.Rows, m1.Columns);
+			for (int row = 0; row < m1.Rows; row++)
+			{
+				for (int col = 0; col < m1.Columns; col++)
+				{
+					result.Values[row, col] = m1.Values[row, col] + m2.Values[row, col];
+				}
+			}
+
+			return result;
+		}
+
+		public static Matrix operator -(Matrix m1, Matrix m2)
+		{
+			if (m1.Rows != m2.Rows || m1.Columns != m2.Columns)
+			{
+				throw new Exception("Matrixes are not the same size");
+			}
+			var result = new Matrix(m1.Rows, m1.Columns);
+			for (int row = 0; row < m1.Rows; row++)
+			{
+				for (int col = 0; col < m1.Columns; col++)
+				{
+					result.Values[row, col] = m1.Values[row, col] - m2.Values[row, col];
+				}
+			}
+
+			return result;
+		}
+
+		public static Matrix operator *(float value, Matrix matrix)
+		{
+			var result = new Matrix(matrix.Rows, matrix.Columns);
+			for (int row = 0; row < matrix.Rows; row++)
+			{
+				for (int col = 0; col < matrix.Columns; col++)
+				{
+					result.Values[row, col] = matrix.Values[row, col] * value;
+				}
+			}
+
+			return result;
+		}
+
+		public static Matrix operator *(Matrix matrix, float value)
+		{
+			return value * matrix;
+		}
+
+		public static Matrix operator *(Matrix m1, Matrix m2)
+		{
+			return new Matrix(1);
+
+		}
+		public Vector ColumnToVector(int column)
+		{
+			if (column >= Columns)
+			{
+				throw new Exception("Column not available");
+			}
+			var vector = new Vector(Rows);
+			for (int i = 0; i < Rows; i++)
+			{
+				vector.Values[i] = Values[i, column];
+			}
+			return vector;
+		}
+
+		public Vector RowToVector(int row)
+		{
+			if (row >= Rows)
+			{
+				throw new Exception("Row not available");
+			}
+			var vector = new Vector(Columns);
+			for (int i = 0; i < Columns; i++)
+			{
+				vector.Values[i] = Values[row, i];
+			}
+			return vector;
+		}
+
+		public static Matrix IdentityMatrix(int size)
+		{
+			var result = new Matrix(size);
+			for (int i = 0; i < size; i++)
+			{
+				result.Values[i, i] = 1;
+			}
+			return result;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
