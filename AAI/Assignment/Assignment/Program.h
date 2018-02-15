b36f5b9e2fdafc48ae1ffc1a71b5f76f9b916670
Sjoerd Dekker <sjoerdherxen@outlook.com>
2018-02-15 11:25:05 +0100

    added aai assignment

---
+++ AAI/Assignment/Assignment/Program.h
@@ -0,0 +1,16 @@
+#ifndef PROGRAM_H
+#define PROGRAM_H
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+#include <GL\glew.h>
+#include <GLFW\glfw3.h>
+#include <glm\glm.hpp>
+
+#include <common/shader.hpp>
+
+void init();	
+
+#endif
