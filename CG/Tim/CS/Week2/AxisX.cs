a83b6ea056167eef4c6ba9fb20d96651fb029229
jjkramok <jjkramok@gmail.com>
2018-02-16 15:26:38 +0100

    added files from elo to my project

---
+++ CG/Tim/CS/Week2/AxisX.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace MatrixTransformations
+{
+    public class AxisX
+    {
+        private int size;
+
+        public List<Vector> vb;
+
+        public AxisX(int size=100)
+        {
+            this.size = size;
+
+            vb = new List<Vector>();
+            vb.Add(new Vector(0, 0));
+            vb.Add(new Vector(size, 0));
+        }
+
+        public void Draw(Graphics g, List<Vector> vb)
+        {
+            Pen pen = new Pen(Color.Red, 2f);
+            g.DrawLine(pen, vb[0].x, vb[0].y, vb[1].x, vb[1].y);
+            Font font = new Font("Arial", 10);
+            PointF p = new PointF(vb[1].x, vb[1].y);
+            g.DrawString("x", font, Brushes.Red, p);
+        }
+    }
+}
---
+++ CG/Tim/CS/Week2/AxisY.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace MatrixTransformations
+{
+    public class AxisY
+    {
+        private int size;
+
+        public List<Vector> vb;
+
+        public AxisY(int size = 100)
+        {
+            this.size = size;
+
+            vb = new List<Vector>();
+            vb.Add(new Vector(0, 0));
+            vb.Add(new Vector(0, size));
+        }
+
+        public void Draw(Graphics g, List<Vector> vb)
+        {
+            Pen pen = new Pen(Color.Green, 2f);
+            g.DrawLine(pen, vb[0].x, vb[0].y, vb[1].x, vb[1].y);
+            Font font = new Font("Arial", 10);
+            PointF p = new PointF(vb[1].x, vb[1].y);
+            g.DrawString("y", font, Brushes.Green, p);
+        }
+    }
+}
