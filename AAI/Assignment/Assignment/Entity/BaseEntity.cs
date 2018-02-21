a683ca42591f792a70baa03d781d1f472937e2aa
Sjoerd Dekker <sjoerdherxen@outlook.com>
2018-02-21 13:22:18 +0100

    added assignment

---
+++ AAI/Assignment/Assignment/Entity/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using Assignment.Movement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Entity
+{
+	abstract class BaseEntity
+	{
+		protected List<BaseSteering> SteeringBehaviours;
+
+	}
+}
---
+++ AAI/Assignment/Assignment/Movement/BaseSteering.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment.Movement
+{
+	abstract class BaseSteering
+	{
+		public double Priority;
+
+		public BaseSteering()
+		{
+
+		}
+
+		public void Calculate()
+		{
+
+		}
+	}
+}
