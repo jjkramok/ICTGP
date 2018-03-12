913908bfae45af23f2c2ed0b0eb8ac10f2d2c8ce
jjkramok <jjkramok@gmail.com>
2018-03-12 14:28:57 +0100

    working on Cpp practice exam

--- CPP/Tim/PracticeExam/BST.h
+++ CPP/Tim/PracticeExam/BST.h
@@ -5,17 +5,45 @@
 #ifndef TIM_BST_H
 #define TIM_BST_H
 
+#include <iostream>
+
+using namespace std;
+
+// Node.info -1 is used as null value for a node.
+
 struct Node {
     int info;
     Node* left;
     Node* right;
+    ~Node() {
+        // Post-order traversal
+        delete left;
+        delete right;
+        //self
+    }
+    void traverse() {
+        // LNR traversal to achieve printing the tree from smallest to biggest info.
+        if (left != nullptr) {
+            left->traverse();
+        }
+        cout << info << " " << endl;
+        if (right != nullptr) {
+            right->traverse();
+        }
+    }
+
 };
 
 class BST {
 public:
-    Node* root;
-    BST();
+    //Node* root; // only here for debug
+    BST(int x);
     ~BST();
+    bool insert(int x);
+    void traverse();
+
+private:
+    Node* root;
 };
 
 
--- CPP/Tim/PracticeExam/Program.cpp
+++ CPP/Tim/PracticeExam/Program.cpp
@@ -3,9 +3,20 @@
 //
 
 #include <iostream>
+#include "BST.h"
 
 using namespace std;
 
 int main() {
+    BST* bst = new BST(7);
+    bst->insert(3);
+    bst->insert(2);
+    bst->insert(5);
+    bst->insert(13);
+    bst->insert(9);
+    bst->insert(17);
+    bst->traverse();
+    delete bst;
+    bst->traverse();
 
 }
