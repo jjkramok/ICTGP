8f3ab1a8a60b27e21306e7307ac1d1cac4b03878
jjkramok <jjkramok@gmail.com>
2018-03-15 10:43:03 +0100

    Added guards to Direction so it remains withing the circumference of the unit circle.

--- AAI/Assignment/Assignment/World/BaseObject.cs
+++ AAI/Assignment/Assignment/World/BaseObject.cs
@@ -7,9 +7,22 @@ using System.Threading.Tasks;
 
 namespace Assignment.World
 {
-	public abstract class BaseObject
-	{
-		public Location Location;
-		public double Direction;
-	}
+    public abstract class BaseObject
+    {
+        private double _direction;
+
+        public Location Location;
+        public double Direction
+        {
+            set
+            {
+                _direction = value;
+                _direction %= 2 * Math.PI;
+            }
+            get
+            {
+                return _direction;
+            }
+        }
+    }
 }
