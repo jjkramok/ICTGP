fdd5aa5c49825f955162c83fc2d23bfb2a72e684
jjkramok <jjkramok@gmail.com>
2018-04-06 13:54:15 +0200

    working on camera and view

--- CG/FinalAssignmentTim/src/main.cpp
+++ CG/FinalAssignmentTim/src/main.cpp
@@ -1,7 +1,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <detail/type_mat2x2.hpp>
+#include <glm.hpp>
 #include <gtc/matrix_transform.hpp>
+#include <stdio.h>
 #include "MyMacros.h"
 #include "Renderer.h"
 #include "VertexBuffer.h"
@@ -12,6 +13,87 @@
 #include "Texture.h"
 
 using namespace std;
+
+/* Typedefs*/
+struct Object {
+    VertexArray vao;
+
+    glm::mat4 model;
+    glm::mat4 model_view;
+};
+
+enum ViewMode {None = -1, Walking = 0, BirdsEye = 1};
+struct Camera {
+    glm::vec3 pos;
+    glm::vec2 direction;
+};
+
+/* Constants and global variables */
+const int Width = 960, Height = 540;
+ViewMode mode = Walking;
+Camera camera = {glm::vec3(0.0f, 0.0f, 0.0f), glm::vec2(glm::radians(45.0f), 0.0f)};
+
+glm::mat4 mvp;
+glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float) Width / (float) Height, 0.1f, 100.0f); /* fov / zoom, aspect ratio, near clipping plane, far clipping plane*/
+//glm::mat4 proj = glm::ortho(0.0f, Width * 1.0f, 0.0f, Height * 1.0f, -1.0f, 1.0f); /* left edge, right edge, bottom edge, top edge, near plane, far plane. Everything outside will be culled */
+glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+const unsigned int amountOfObjects = 10;
+Object *objects;
+
+const float speed = 1.0f;
+
+/* Main functions */
+void HandleKeyPress(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwDestroyWindow(window);
+    } else if (key == GLFW_KEY_C && action == GLFW_RELEASE) {
+        if (mode == Walking) {
+            mode = BirdsEye;
+            camera.pos = glm::vec3(0.0f, 0.0f, 0.0f);
+            camera.direction.x = glm::radians(-45.0f);
+            camera.direction.y = glm::radians(-45.0f);
+        } else {
+            mode = Walking;
+            camera.pos = glm::vec3(0.0f, 0.0f, 0.0f);
+            camera.direction.x = glm::radians(-90.0f);
+            camera.direction.y = 0;
+        }
+        cout << "changed mode to: " << (mode ? "Walking" : "Birds Eye") << endl;
+    } else if (key == GLFW_KEY_W && action == GLFW_REPEAT) {
+        camera.pos.x += cos(camera.direction.x) * speed;
+        camera.pos.z += sin(camera.direction.x) * speed;
+    } else if (key == GLFW_KEY_A && action == GLFW_REPEAT) {
+        camera.pos.x += cos(camera.direction.x + glm::radians(-90.0f)) * speed;
+        camera.pos.z += sin(camera.direction.x + glm::radians(-90.0f)) * speed;
+    } else if (key == GLFW_KEY_S && action == GLFW_REPEAT) {
+        camera.pos.x += cos(camera.direction.x + glm::radians(180.0f)) * speed;
+        camera.pos.z += sin(camera.direction.x + glm::radians(180.0f)) * speed;
+    } else if (key == GLFW_KEY_D && action == GLFW_REPEAT) {
+        camera.pos.x += cos(camera.direction.x + glm::radians(90.0f)) * speed;
+        camera.pos.z += sin(camera.direction.x + glm::radians(90.0f)) * speed;
+    }
+
+    cout << "camera at (x, y, z): " << camera.pos.x << ", " <<  camera.pos.y << ", " <<  camera.pos.z << endl;
+}
+
+/**
+ * Updates the global view matrix and the the model view matrix of all objects to match the new camera state.
+ */
+void UpdateCamera() {
+    glm::vec3 target = glm::vec3(0.0f, 0.0f, 0.0f);
+    target.x = cos(camera.direction.x) * (glm::radians(90.0f) - abs(camera.direction.y)) + camera.pos.x;
+    target.z = sin(camera.direction.x) * (glm::radians(90.0f) - abs(camera.direction.y)) + camera.pos.z;
+    float d = (float) sqrt(pow(target.x - camera.pos.x, 2) + pow(target.z - camera.pos.z, 2));
+    target.y = tan(camera.direction.y) * d + camera.pos.y;
+
+    view = glm::lookAt(camera.pos, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+
+    for (int i = 0; i < amountOfObjects; i++) {
+        objects[i] .model_view = view * objects[i].model;
+    }
+}
 
 int main() {
     GLFWwindow *window;
@@ -27,7 +109,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Create the main window.
-    window = glfwCreateWindow(860, 720, "Final Assignment", nullptr, nullptr);
+    window = glfwCreateWindow(Width, Height, "Final Assignment", nullptr, nullptr);
     if (!window) {
         glfwTerminate();
         return -1;
@@ -45,16 +127,20 @@ int main() {
     printf("OpenGL version supported by this platform (%s): \n",
            glGetString(GL_VERSION));
 
+    glfwSetKeyCallback(window, HandleKeyPress);
+
+    objects = new Object[amountOfObjects];
+
     /*
      *  Scope that ends just before destroying the GL rendering context.
      *  This way any stack allocated objects will be destroyed before the context will be.
      **/
     {
         float pos[] = {
-                -0.5f, -0.5f, 0.0f, 0.0f, /* bottom left*/
-                0.5f, -0.5f, 1.0f, 0.0f, /* bottom right */
-                0.5f, 0.5f, 1.0f, 1.0f, /* top right */
-                -0.5f, 0.5f, 0.0f, 1.0f, /* top left */
+                100.0f, 100.0f, 0.0f, 0.0f, /* bottom left*/
+                200.0f, 100.0f, 1.0f, 0.0f, /* bottom right */
+                200.0f, 200.0f, 1.0f, 1.0f, /* top right */
+                100.0f, 200.0f, 0.0f, 1.0f, /* top left */
         };
         unsigned int is[6] = {
                 0, 1, 2,
@@ -74,7 +160,7 @@ int main() {
 
         IndexBuffer ibo(is, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f); /* left edge, right edge, bottom edge, top edge, near plane, far plane. Everything outside will be culled */
+        mvp = proj * view * model;
 
         Shader shader("./../res/shaders/Basic.glsl");
         shader.Bind();
@@ -84,7 +170,7 @@ int main() {
         unsigned char textureSlot = 0;
         texture.Bind(textureSlot);
         shader.SetUniform1i("u_Texture", textureSlot);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         vao.Unbind();
         vbo.Unbind();
@@ -99,12 +185,19 @@ int main() {
 
         // Loop until window is closed by the user.
         while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
             renderer.Clear();
+
+            //view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0, 1, 0)); /* camera position, camera target, upVector */
+            //proj = glm::perspective(glm::radians(45.0f), (float) Width / (float) Height, 0.1f, 100.0f); /* fov / zoom, aspect ratio, near clipping plane, far clipping plane*/
+            mvp = proj * view * model;
 
             shader.Bind();
             shader.SetUniform4f("u_Color", glm::vec4(r, 0.3f, 0.8f, 1.0f));
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(vao, ibo, shader);
+
 
             if (r > 1.0f) {
                 increment = -increment;
@@ -117,6 +210,9 @@ int main() {
             glfwPollEvents();
         }
     }
+
+    /* Destroy all (non-stack) allocated memory */
+    delete[] objects;
 
     glfwTerminate();
     return 0;
